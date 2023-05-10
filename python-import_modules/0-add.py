a3bfc9cd8aaf5276fabdfd5c6e48fdc70fffa0c5
Aaron Bell <aaronbll46@gmail.com>
2023-05-10 16:33:20 -0700

    suley

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add as ad:
 if __name__ == "__main__"
-a = 1
-b = 2
-sum = ad(a, b)
-print("{} + {} = {}".format(a, b, sum))
+    a = 1
+    b = 2
+    sum = ad(a, b)
+    print("{} + {} = {}".format(a, b, sum))
