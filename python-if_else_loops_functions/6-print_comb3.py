469fd1779d28017f683d66d2f0b94270df38716d
Aaron Bell <aaronbll46@gmail.com>
2023-05-10 13:05:28 -0700

    try this

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i+1, 10):
-        if i != j:
-            print("{}{}".format(i, j), end='')
-            if i == 8 and j == 9:
-                print()
-            else:
-                print("", end=', ')
+    for j in range(10):
+        if i >= j:
+            break
+        print("{}{}".format(i, j), end='')
+        if i == 8 and j == 9:
+            print()
+        else:
+            print("", end=', ')
