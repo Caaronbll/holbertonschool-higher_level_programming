b7f22250ce5334b27ea0d9944b33d325a0ecc8de
Aaron Bell <aaronbll46@gmail.com>
2023-05-10 15:28:50 -0700

    removed a negative

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     if number >= 0:
         lstNum = number % 10
     if number < 0:
-        lstNum = number % -10
+        lstNum = number % 10
     print(f"{lstNum}", end='')
     return(lstNum)
