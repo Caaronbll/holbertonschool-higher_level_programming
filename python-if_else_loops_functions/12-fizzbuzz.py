24cbe819cdfcebc5698ddeedadc2fec33d81be6b
Aaron Bell <aaronbll46@gmail.com>
2023-05-10 15:53:41 -0700

    work now

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    i = 1
-    for i in range(101):
+    for i in range(1, 101):
         if i % 3 and 1 % 5 is 0:
             print("FizzBuzz", end=' ')
         elif i % 3 == 0:
