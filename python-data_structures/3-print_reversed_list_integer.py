bf8229cfd5d947b816b74a1947a7e7f782971f5f
Aaron Bell <aaronbll46@gmail.com>
2023-05-13 15:00:32 -0700

    added none type

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     t = (len(my_list) - 1)
+    if my_list = None:
+        return None
     while t >= 0:
         print("{:d}".format(my_list[t]))
         t = t - 1
