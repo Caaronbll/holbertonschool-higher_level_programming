72d0b26086cfceb17f85106256b9f06687422572
Aaron Bell <aaronbll46@gmail.com>
2023-05-13 18:27:48 -0700

    adding index

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ''
     for i in range(len(my_string)):
-        if my_string[i] != 'c' and my_string != 'C':
+        if my_string[i] != 'c' and my_string[i] != 'C':
             new_string += my_string[i:(1 + i)]
     return new_string
