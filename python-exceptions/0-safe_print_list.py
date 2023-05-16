5a252c3a5674837bd32923dcdcf3c642e4299840
Aaron Bell <aaronbll46@gmail.com>
2023-05-16 15:26:27 -0700

    work

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    if i in range(x):
+    for i in range(x):
         try:
             print(my_list[i], end='')
             count = count + 1
-        except IndexError:
+        except:
             break
     print()
     return count
