8ce1fa059df745ac8ba46f2b28e21bcee2da451d
Aaron Bell <aaronbll46@gmail.com>
2023-05-16 12:16:56 -0700

    test

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = list(map(lambda x: x * 2, a_dictionary))
+    new_dictionary = []
+    if a_dictionary:
+        for i in a_dictionary:
+            new_dictionary[i] = a_dictionary[i] * 2
     return new_dictionary
