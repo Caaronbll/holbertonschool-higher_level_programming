b28d93a416812906c3d5f586eefa2de24ff167d0
Aaron Bell <aaronbll46@gmail.com>
2023-05-16 09:57:21 -0700

    idk if it will wortk

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if !sentence:
+        first_char = None
+        length = 0
+    else:
+        first_char = sentence[0]
+        length = len(sentence)
+    return length first_char
