ca427f11d078fb35e7ba24149f8c58a4b311114d
Aaron Bell <aaronbll46@gmail.com>
2023-05-16 12:13:32 -0700

    try this

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        new_dictionary[i] = a_dictionary[i] * 2
+    new_dictionary = list(map(lambda x: x * 2, a_dictionary))
     return new_dictionary
