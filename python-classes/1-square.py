b02da58a830cb831883cb8f08236f3b0fe062775
Aaron Bell <aaronbll46@gmail.com>
2023-05-19 17:33:19 -0700

    q1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defining a class with properties"""
+
+
+class Square:
+    """Class declaration"""
+    def __init__(self, size):
+        """Mandatory init method.
+
+        Args:
+            size (str): size of the square
+        """
+        self.size = size
