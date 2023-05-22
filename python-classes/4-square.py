2d8271c8521007f3a741f9a768162ff1cbb3bf26
Aaron Bell <aaronbll46@gmail.com>
2023-05-22 12:24:29 -0700

    should work now

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,13 +5,19 @@
 class Square:
     """Declaring Square Class"""
     def __init__(self, size=0):
-        """Declaring size attribute"""
         self.__size = size
-        if type(self.__size) is not int:
-            raise TypeError("size must be an integer")
-        if self.__size < 0:
-            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
 
     def area(self):
-        """Declaring area method"""
         return self.__size * self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
