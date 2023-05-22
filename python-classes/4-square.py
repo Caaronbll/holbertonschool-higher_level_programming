e5adb278007416e883432fe751be8191e2b7ce82
Aaron Bell <aaronbll46@gmail.com>
2023-05-22 12:13:13 -0700

    test

---
+++ python-classes/4-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square Class set-up"""
+
+
+class Square:
+    """Declaring Square Class"""
+    def __init__(self, size=0):
+        """Declaring size attribute"""
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Declaring area method"""
+        return self.__size * self.__size
