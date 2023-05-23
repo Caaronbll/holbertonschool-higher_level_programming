ea21516a984ff894a2eba8e471f36083ee8eb5ee
Caaronbll <aaronbll46@gmail.com>
2023-05-23 12:30:26 -0700

    git me more points

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+says first and last name based on 2 string inputs
+"""
+
 
 def say_my_name(first_name, last_name=""):
+    """Makes sure both inputs are strings and prints them"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
