deae88d835a87cee1d498bda6c2b5e65287eec04
Aaron Bell <aaronbll46@gmail.com>
2023-05-23 07:19:13 -0700

    test failed

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,10 +4,14 @@ Adds two integers and makes sure both integers
 are integers or floats before they are added.
 """
 def add_integer(a, b=98):
-    if type(a) is not float or type(a) is not int:
-        raise TypeError("a must be an integer")
-    if type(b) is not float or type(b) is not int:
-        raise TypeError("b must be an integer")
-    a_int = int(a)
-    b_int = int(b)
-    return a_int + b_int
+    if type(a) is not int:
+        if type(a) is float:
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+    if type(b) is not int:
+        if type(b) is float:
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+    return a + b
