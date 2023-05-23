c450c3c8bb35cdb163bff738e3d7f575bf038185
Aaron Bell <aaronbll46@gmail.com>
2023-05-23 07:49:23 -0700

    added position setter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -27,6 +27,13 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or type(value[0]) is not int \
+        or type(value[1]) is not int or value[0] < 0 or value[1] \
+        < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
     def my_print(self):
         if self.__size == 0:
             print()
