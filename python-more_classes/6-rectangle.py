e6b958d362ae6c0a7c860de647fd09f25363e0e1
Caaronbll <aaronbll46@gmail.com>
2023-05-24 08:50:46 -0700

    instance

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,6 +6,9 @@ writing a rectangle class
 
 class Rectangle:
     """Declaring a Rectangle class"""
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
