50484e30aea5b2b3e0b78d699a4d6cf02edec77b
Caaronbll <aaronbll46@gmail.com>
2023-05-23 19:19:57 -0700

    please

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle:
     def __str__(self):
         string = ""
         if self.__width == 0:
-            print("\n")
+            print("")
         elif self.height > 0 and self.height > 0:
             for row in range(self.height):
                 for col in range(self.width):
