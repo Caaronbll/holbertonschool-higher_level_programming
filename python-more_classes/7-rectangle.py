ac425b54e14718b244ee46ba8fb41da543f9b56b
Caaronbll <aaronbll46@gmail.com>
2023-05-24 09:51:15 -0700

    print symbol

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+writing a rectangle class
+"""
+
+
+class Rectangle:
+    """Declaring a Rectangle class"""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """getter for width property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for width property"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """getter for height property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height property"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Public instance method Area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Public instance method perimeter"""
+        if self.__width == 0:
+            return 0
+        elif self.__height == 0:
+            return 0
+        else:
+            p = (self.__width + self.__height) * 2
+            return p
+
+    def __str__(self):
+        string = ""
+        if self.height > 0 and self.height > 0:
+            for row in range(self.height):
+                for col in range(self.width):
+                    string += str(self.print_symbol)
+                if row < self.height - 1:
+                    string += '\n'
+            return string
+        else:
+            return string
+
+    def __repr__(self) -> str:
+        """repr/eval function "giver" """
+        return f"Rectangle({self.__width}, {self._height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
