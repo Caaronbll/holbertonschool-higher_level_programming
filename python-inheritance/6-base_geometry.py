098110b266e943e3409b5c6e09291429c0abd9c4
Caaronbll <aaronbll46@gmail.com>
2023-05-29 13:55:03 -0700

    test q6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write an empty class BaseGeometry"""
+
+
+class BaseGeometry():
+    """class declaration"""
+
+    def area(self):
+        raise Exception("area() is not implementd")
