3c4c17036f61b6f35549e6ad0aa226b8aff0131b
Caaronbll <aaronbll46@gmail.com>
2023-05-29 13:44:39 -0700

    test q4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,7 +10,7 @@ specified class ; otherwise False
 
 def inherits_from(obj, a_class):
     """Definition"""
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and obj is not a_class:
         return True
     else:
         return False
