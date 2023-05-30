627df9561a98d6ad628a1de9a7be7ef65ad13e78
Caaronbll <aaronbll46@gmail.com>
2023-05-29 19:36:20 -0700

    please work

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Write a class Square that inherits from Rectangle
-size: def __init__(self, size)"""
-
+"""
+Write a class Square that inherits from Rectangle
+"""
 
 Rectangle = __import__('9-rectangle.py').Rectangle
+"""imported module"""
 
 
 class Square(Rectangle):
