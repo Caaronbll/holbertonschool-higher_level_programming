2d5b4f8105954fc3fe2beac1297c32393e9e85cd
Caaronbll <aaronbll46@gmail.com>
2023-05-30 10:19:34 -0700

    test q6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a function that
+creates an Object from
+a “JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates obj from jsonfile"""
+    with open(filename, 'r') as f:
+        return json.load(f)
