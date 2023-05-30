c752bd6e6bfd0e496d3c125403c1844f55280dd4
Caaronbll <aaronbll46@gmail.com>
2023-05-29 18:55:56 -0700

    added blank line

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,6 +2,8 @@
 """
 Write a class Square that inherits from Rectangle
 """
+
+
 Rectangle = __import__('9-rectangle.py').Rectangle
 
 
@@ -12,7 +14,3 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        """area method"""
-        return self.__size * self.__size
