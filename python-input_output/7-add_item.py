d45bff624bfdf7aba146e20e97322fcb697cbe2a
Caaronbll <aaronbll46@gmail.com>
2023-05-30 12:07:20 -0700

    work

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,3 +12,4 @@ if __name__ == "__main__":
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     items = load_from_json_file("add_item.json")
+    save_to_json_file(items, "add_item.json")
