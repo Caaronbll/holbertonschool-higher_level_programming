9bc35570e3f41a05c7eb3e4403f812a7c031b4c6
Caaronbll <aaronbll46@gmail.com>
2023-05-29 19:39:41 -0700

    mine

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""Square class that inherits from Rectangle (9-rectangle.py)
-Instantiation with size: def __init__(self, size)
-size must be private. No getter or setter
-size must be a positive integer, validated by integer_validator
-the area() method must be implemented
-print() should print, and str() should return, the square description:
-[Square] <width>/<height>"""
+"""
+Square class that inherits from Rectangle
+"""
 
 Rectangle = __import__('9-rectangle.py').Rectangle
 
