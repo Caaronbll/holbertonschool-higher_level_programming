6191f71b574f584082931cf511ecc9a9b6c2f2ab
Caaronbll <aaronbll46@gmail.com>
2023-05-29 18:48:22 -0700

    try this

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,7 +2,6 @@
 """
 Write a class Square that inherits from Rectangle
 """
-
 Rectangle = __import__('9-rectangle.py').Rectangle
 
 
@@ -10,6 +9,7 @@ class Square(Rectangle):
     """Declaration"""
     def __init__(self, size):
         """initialization"""
+        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
 
