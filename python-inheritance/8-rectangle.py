ecf5db7a661fa7a3d727f40726dbff407a0d766c
Caaronbll <aaronbll46@gmail.com>
2023-05-29 18:24:59 -0700

    q8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-"""Write an empty class BaseGeometry"""
+"""
+Write a class Rectangle that inherits from BaseGeometry
+"""
 
-
-class BaseGeometry():
-    """class declaration"""
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """class declaration"""
