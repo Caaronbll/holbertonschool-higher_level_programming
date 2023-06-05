2d427d410c606957d07c005623b8892ba2d16587
Caaronbll <aaronbll46@gmail.com>
2023-06-05 15:56:22 -0700

    q10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Write the class Square that inherits from Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """initializing Square Class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """returns information about the square"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
