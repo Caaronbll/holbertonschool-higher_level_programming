cd2b650f7f65527584faa72a96639ea408b839a1
Caaronbll <aaronbll46@gmail.com>
2023-06-05 09:57:07 -0700

    test

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,28 +9,8 @@ class Rectangle(Base):
     """rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """named module"""
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
         super().__init__(id)
-
-    @property
-    def width(self):
-        """width setter"""
-        return self.__width
-
-    @property
-    def height(self):
-        """height setter"""
-        return self.__height
-
-    @property
-    def x(self):
-        """y setter"""
-        return self.__x
-
-    @property
-    def y(self):
-        """x setter"""
-        return self.__y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
