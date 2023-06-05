4be4dca77bedbbd02a6bf4a18f4ba7365f9529d0
Caaronbll <aaronbll46@gmail.com>
2023-06-05 16:23:18 -0700

    test q14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -24,3 +24,7 @@ class Square(Rectangle):
     def __str__(self):
         """returns information about the square"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def to_dictionary(self):
+        """returns a dictionary for square class"""
+        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
