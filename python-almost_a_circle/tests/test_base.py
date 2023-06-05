983eb6b4dfcd9b7514bf48e2a4b68a0af10ec59c
Caaronbll <aaronbll46@gmail.com>
2023-06-05 10:20:22 -0700

    test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -7,5 +7,13 @@ class TestBase(unittest.TestCase):
     """Testing the Base Class"""
 
     def setUp(self):
+        """imports the variable"""
         Base.__nb_objects = 0
         pass
+
+    def tearDown(self):
+        """Clean up code"""
+        pass
+
+    def test_A_nb_objects_private(self):
+        self.assertTrue(hasattr(Base, "_Base__nb_objects"))
