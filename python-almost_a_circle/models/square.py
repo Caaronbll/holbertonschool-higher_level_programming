cc49e0d34f4331b90955feb94717a0b2c34ee438
Caaronbll <aaronbll46@gmail.com>
2023-06-06 10:43:38 -0700

    args for square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,3 +28,21 @@ class Square(Rectangle):
     def to_dictionary(self):
         """returns a dictionary for square class"""
         return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
+
+    def update(self, *args, **kwargs):
+        """updates attributes with and without keywords"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """updates the attributes"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
