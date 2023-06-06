fb126c2ed7b7a73420e3068e4b7292ff607f8cc9
Caaronbll <aaronbll46@gmail.com>
2023-06-06 06:11:35 -0700

    test

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,7 +22,7 @@ class Rectangle(Base):
     def __str__(self):
         """returns information of this rectangle"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y}\
-                - {self.width}/{self.height}"
+        - {self.width}/{self.height}"
 
     @property
     def width(self):
