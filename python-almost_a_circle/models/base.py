e793eeac12043f8af2a7f57108ddb6113b4b8b7b
Caaronbll <aaronbll46@gmail.com>
2023-06-06 06:26:30 -0700

    a lot

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Write the first class Base"""
 import json
 
+
 class Base():
     """Base Class"""
     __nb_objects = 0
@@ -13,9 +14,9 @@ class Base():
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns json string rep of list_dict"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
