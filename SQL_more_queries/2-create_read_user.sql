efd2caaba89310b3308814cd1b2f286a58eedeba
Caaronbll <aaronbll46@gmail.com>
2023-06-26 13:20:43 -0700

    test

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,2 +1,3 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2
-CREATE hbtn_0d_2 CREATE USER user_0d_2;
+CREATE hbtn_0d_2 IF NOT EXISTS
+CREATE USER user_0d_2 IDENTIFIED BY user_0d_2_pwd;
