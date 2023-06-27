83d20cfbc01adcf3cd73ba3c7edeaf3897cd5787
Caaronbll <aaronbll46@gmail.com>
2023-06-27 10:54:59 -0700

    push

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2
-CREATE USER 'user_0d_2'@ IDENTIFIED BY user_0d_2_pwd;
+CREATE USER 'user_0d_2'@ IDENTIFIED BY user_0d_2_pwd
+GRANT SELECT TO 'user_0d_2';
