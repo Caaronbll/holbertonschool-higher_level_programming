65b943ee5fb4041fec09970bc3c9af368dd46de6
Caaronbll <aaronbll46@gmail.com>
2023-06-27 19:25:43 -0700

    dexter genres

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,4 +2,4 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres JOIN tv_show_genres ON tv_genres.ID = tv_show_genres.genre_id
 GROUP BY tv_genres.name, tv_show_genres.genre_id
-ORDER BY DESC number_of_shows;
+ORDER BY number_of_shows DESC;
