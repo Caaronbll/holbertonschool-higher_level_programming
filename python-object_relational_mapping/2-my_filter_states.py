ee7c61feee849d597a8ecdf442ccaee534a5e2df
Caaronbll <aaronbll46@gmail.com>
2023-07-02 09:49:26 -0700

    q2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                                passwd=sys.argv[2], db=sys.argv[3],
                                charset="utf8")
     cur = db_cnctn.cursor()
-    cur.execute("SELECT * FROM states WHERE\
-                BINARY name = {}".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states\
+                WHERE BINARY name = {}".format(sys.argv[4]))
     for state in cur.fetchall():
         print(f"{state}")
