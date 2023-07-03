d0e1eb154d3d70e20315caff9a705dcc25dd73da
Caaronbll <aaronbll46@gmail.com>
2023-07-03 07:22:51 -0700

    q2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,8 +12,6 @@ if __name__ == "__main__":
     cur = db_cnctn.cursor()
     cur.execute("SELECT * FROM 'states'\
                 WHERE BINARY 'name' = '{}' ORDER BY id ASC".format(sys.argv[4]))
-    for state in cur.fetchall():
-        print(f"{state}")
+    for row in cur.fetchall():
+        print(f"{row}")
 
-    cur.close()
-    db_cnctn.close()
