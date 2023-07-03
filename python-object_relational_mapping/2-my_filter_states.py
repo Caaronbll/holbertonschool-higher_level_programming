574346ff7bbda0d0da0759f4f20440e9828c7a3f
Caaronbll <aaronbll46@gmail.com>
2023-07-03 07:53:03 -0700

    semicolin

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """script that takes in an argument and displays all values in the states"""
 
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    """documentation"""
+    """main function"""
+
     db_cnctn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3],
                                charset="utf8")
-    cur = db_cnctn.cursor()
-    cur.execute("SELECT * FROM 'states'\
-                WHERE BINARY 'name' = '{}' ORDER BY id ASC".format(sys.argv[4]))
-    for row in cur.fetchall():
+    db_connect = db_cnctn.cursor()
+    db_connect.execute("SELECT * FROM 'states'\
+                WHERE BINARY 'name' = '{}' ORDER BY id ASC;".format(sys.argv[4]))
+    for row in db_connect.fetchall():
         print(f"{row}")
 
