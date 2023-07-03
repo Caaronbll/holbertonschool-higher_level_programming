88e41c3584a9365262acc813a27d531149d18078
Caaronbll <aaronbll46@gmail.com>
2023-07-03 09:20:55 -0700

    test

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -16,5 +16,6 @@ if __name__ == "__main__":
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
-
-    print(", ".join([row[2] for row in db_connect.fetchall() if row[4] == sys.argv[4]]))
+    for row in db_connect.fetchall():
+        if row[4] == sys.argv[4]:
+            print(", ".join([row[2]]))
