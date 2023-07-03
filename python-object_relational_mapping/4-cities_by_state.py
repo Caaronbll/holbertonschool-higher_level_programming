b9301c395899ec1e60f37749fd39318e6a8d8381
Caaronbll <aaronbll46@gmail.com>
2023-07-03 08:48:25 -0700

    push

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -6,15 +6,13 @@ import MySQLdb
 
 if __name__ == "__main__":
     """documentation"""
+
     db_cnctn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3],
                                charset="utf8")
-    cur = db_cnctn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name\
-                FROM cities, states WHERE cities.state_id = states.id\
-                ORDER BY cities.id ASC")
-    for state in cur.fetchall():
-        print(f"{state}")
 
-        cur.close()
-        db_cnctn.close()
+    db_connect = db_cnctn.cursor()
+    db_connect.execute("SELECT * FROM states WHERE name = %s ORDER BY id")
+
+    for state in db_connect.fetchall():
+        print(f"{state}")
