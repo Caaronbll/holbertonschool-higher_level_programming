d24f34f7343111a371b8940c03ee133ab89ae0b3
Caaronbll <aaronbll46@gmail.com>
2023-07-04 15:41:07 -0700

    8 better work

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""script that lists all State objects from the database hbtn_0e_6_usa"""
-
+""" Start link class to table in database """
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
+    first = session.query(State).order_by(State.id).first()
 
-    query = session.query(State).first()
-
-    if query is None:
-        print("Nothing")
+    if first:
+        print("{}: {}".format(first.id, first.name))
     else:
-        print("{}: {}".format(query.id, query.name))
+        print("Nothing")
 
     session.close()
