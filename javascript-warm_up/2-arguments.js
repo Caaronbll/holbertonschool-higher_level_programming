9a512ceb3e1f653a2fb3e3826e65ba8dccdd3f07
Christopher <christopher@ChristophersMBP.attlocal.net>
2023-07-28 15:23:58 -0500

    added thripple equal

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,11 +2,11 @@
 
 const argcount = process.argv.length;
 
-if (argcount == 0){
+if (argcount === 0){
   console.log('No argument');
 }
-else if (argcount == 1){
-  console.log('No argument');
+else if (argcount === 1){
+  console.log('Argument found');
 }
 else if (argcount > 1){
   console.log('Arguments found')
