7cd7e8e0cb5713c30ecea989904f62092260859a
Christopher <christopher@ChristophersMBP.attlocal.net>
2023-07-28 14:47:03 -0500

    adding permission

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,15 +4,15 @@ import { argv } from 'node:process';
 
 let argcount = 0;
 argv.forEach((val, index) => {
-    argcount =+ 1;
-  });
+  argcount =+ 1;
+});
 
 if (argcount == 0){
-    console.log('No argument');
+  console.log('No argument');
 }
 else if (argcount == 1){
-    console.log('No argument');
+  console.log('No argument');
 }
 else if (argcount > 1){
-    console.log('Arguments found')
+  console.log('Arguments found')
 }
