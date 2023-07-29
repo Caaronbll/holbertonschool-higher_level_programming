6ff21fbb88cf66c58ce09ac99450ac93d6f42f8b
Christopher <christopher@ChristophersMBP.attlocal.net>
2023-07-29 13:35:06 -0500

    task 4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,15 +1,3 @@
 #!/usr/bin/node
 
-const arguments = proccess.argv.slice(2);
-let arg1, arg2;
-if (arguments[0]) {
-    arg1 = arguments[0];
-} else {
-    arg1 = 'undefined';
-}
-if (arguments[1]) {
-    arg2 = arguments[1];
-} else {
-    arg2 = 'undefined';
-}
-console.log(arg1 + ' is ' + arg2);
+console.log(proccess.argv[2] + ' is ' + proccess.argv[3]);
