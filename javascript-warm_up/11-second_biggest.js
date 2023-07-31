a7d4df5936948fd961a2d6f1775920b43dec80b0
Christopher <christopher@Christophers-MacBook-Pro.local>
2023-07-31 11:11:44 -0500

    q11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+function findSecondBiggestInteger(...args) {
+  if (args.length <= 1) {
+    return 0;
+  }
+
+  let biggest = -Infinity;
+  let secondBiggest = -Infinity;
+
+  for (const arg of args) {
+    const num = parseInt(arg);
+
+  if (num > biggest) {
+      secondBiggest = biggest;
+      biggest = num;
+    } else if (num > secondBiggest && num !== biggest) {
+      secondBiggest = num;
+    }
+  }
+
+  return secondBiggest;
+}
+
+  // Get command-line arguments and pass them to the function
+  const args = process.argv.slice(2);
+  const secondBiggest = findSecondBiggestInteger(...args);
+
+  // Print the result
+  console.log(secondBiggest);
