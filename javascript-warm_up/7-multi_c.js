c5a80c1efbb04b3531513391d7e10a330270bce8
Christopher <christopher@Christophers-MacBook-Pro.local>
2023-07-31 10:27:57 -0500

    adding for semistandard

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ Number(x[0]);
 if (isNaN(x[0])) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x[0]; i++){
+  for (let i = 0; i < x[0]; i++) {
     console.log('C is fun');
-  }
+  };
 }
