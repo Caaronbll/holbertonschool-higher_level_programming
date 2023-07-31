6cf39e846667711d78909f4a0df0b8d9add2f217
Christopher <christopher@Christophers-MacBook-Pro.local>
2023-07-31 10:41:28 -0500

    fixing spaces

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,12 +4,12 @@ const arg = process.argv.slice(2);
 const size = arg[0];
 let outline = '';
 if (isNaN(size)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let y = 0; y < size; y++) {
-        outline += 'X';
-    }
-    for (let x = 0; x < size; x++) {
-        console.log(outline);
-    }
+  for (let y = 0; y < size; y++) {
+    outline += 'X';
+  }
+  for (let x = 0; x < size; x++) {
+    console.log(outline);
+  }
 }
