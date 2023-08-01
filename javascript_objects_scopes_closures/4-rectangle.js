f522741f9450993a9c6acc8282fc97269ff643ab
Christopher <christopher@ChristophersMBP.attlocal.net>
2023-08-01 16:00:47 -0500

    q4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let count = '';
+    for (let x = 0; x < this.width; x++) {
+      count += 'X';
+    }
+    for (let y = 0; y < this.height; y++) {
+      console.log(count);
+    }
+  }
+
+  rotate () {
+    let holder = this.height;
+    this.height = this.width;
+    this.width = holder;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+module.exports = Rectangle;
